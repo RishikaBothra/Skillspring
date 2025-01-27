d08ca0feaa676fd76376bb98651a9395236678c0
RishikaBothra <rshkbothra@gmail.com>
2025-01-27 22:04:49 +0530

    added the signup page frontend

--- packages/Frontend/src/App.jsx
+++ packages/Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
+import Signup from './pages/signup';
 import Main from './pages/mainpg';
 import './index.css';
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
